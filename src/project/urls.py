537926a681d443d0c461b1b9c85dc374508f4424
daikidomon <daiki.domon@daikidomon.com>
2021-08-22 12:03:55 +0900

    Add Supervisor

--- src/project/urls.py
+++ src/project/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
+    path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
     path('chat/', include('chat.urls')),
 ]
